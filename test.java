39e2a8fc2ba62b36ea7147e4afc44f4d424cdb2a
problemer <1784012538@qq.com>
2017-11-17 23:19:36 +0800

    xzshd

---
+++ test.java
@@ -0,0 +1,10 @@
+public class test {
+    static   String[] point = {"1","2","3","4","4","6","7","8","9","10","J","Q","K","A"};
+     static String[]  color = {"◇","♥","♣","♠"};
+
+    public static void main(String[] args) {
+        for(int i = 0;i< point.length;i++)
+        System.out.print(point[i]);
+        System.out.print(color[]);
+    }
+}
